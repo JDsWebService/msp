aab8ee257d0314dc1ca4b9b3d60d5d97508cf464
Jonathan Robinson <jdswebservice@gmail.com>
2020-08-28 03:38:22 -0400

    Template: Moved Testimonials Section To Content Snippets & Moved All Content Snippets To Index Page

---
+++ resources/views/content/app/testimonial.blade.php
@@ -0,0 +1,102 @@
+{{-- @include('content.app.testimonial') --}}
+<section class="testimonial">
+    <div class="container">
+        <div class="row">
+            <div class="section-title col-sm-12 text-center">
+                <h2>Fun Facts About Us</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium tenetur, ad fuga nobis facilis sunt repudiandae, voluptatum? Magni, ab possimus beatae esse debitis eius sapiente hic aspernatur quaerat modi numquam.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            {{-- Counters --}}
+            <div class="col-lg-6">
+                <div class="block">
+                    <ul class="counter-box clearfix">
+                        <li>
+                            <div class="counter-item"> <i class="ion-ios-chatboxes-outline"></i>
+                                <h4 class="count" data-count="99">0</h4>
+                                <span>Cups Of Coffee</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="counter-item"> <i class="ion-ios-glasses-outline"></i>
+                                <h4 class="count" data-count="45">0</h4>
+                                <span>Article Written</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="counter-item"> <i class="ion-ios-compose-outline"></i>
+                                <h4 class="count" data-count="125">0</h4>
+                                <span>Projects Completed</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="counter-item"> <i class="ion-ios-timer-outline"></i>
+                                <h4 class="count" data-count="200">0</h4>
+                                <span>Combined Projects</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div> {{-- /.counters --}}
+
+            {{-- Testimonials --}}
+            <div class="col-lg-5 offset-md-1">
+                <div class="testimonial-carousel text-center">
+                    <div class="testimonial-slider owl-carousel">
+                        <div> <i class="ion-quote"></i>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <div class="user">
+                                <img src="images/item-img1.jpg" alt="Pepole">
+                                <p><span>Rose Ray</span> CEO-Themefisher</p>
+                            </div>
+                        </div>
+                        <div> <i class="ion-quote"></i>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <div class="user">
+                                <img src="images/item-img1.jpg" alt="Pepole">
+                                <p><span>Rose Ray</span> CEO-Themefisher</p>
+                            </div>
+                        </div>
+                        <div> <i class="ion-quote"></i>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <div class="user">
+                                <img src="images/item-img1.jpg" alt="Pepole">
+                                <p><span>Rose Ray</span> CEO-Themefisher</p>
+                            </div>
+                        </div>
+                        <div> <i class="ion-quote"></i>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <div class="user">
+                                <img src="images/item-img1.jpg" alt="Pepole">
+                                <p><span>Rose Ray</span> CEO-Themefisher</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,25 @@
 @extends('layouts.app')
 
+@section('title', 'Home')
+
+@section('content')
+
+    {{-- Above The Fold Hero Section --}}
+    @include('content.app.hero')
+
+    {{-- About Us Section --}}
+    @include('content.app.about')
+
+    {{-- Feature Section --}}
+    @include('content.app.feature')
+
+    {{-- Service Section --}}
+    @include('content.app.service')
+
+    {{-- Call To Action Section --}}
+    @include('content.app.calltoaction')
+    
+    {{-- Testimonial Section --}}
+    @include('content.app.testimonial')
+
+@endsection
