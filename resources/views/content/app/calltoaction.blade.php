0eac3ba05566f2dfeae73081d5ffd29b2072aeee
Jonathan Robinson <jdswebservice@gmail.com>
2020-08-30 23:35:23 -0400

    Call To Action Button Hotfix

--- resources/views/content/app/calltoaction.blade.php
+++ resources/views/content/app/calltoaction.blade.php
@@ -6,7 +6,7 @@
                 <div class="block">
                     <h2>High Quality Affordable Aerial Photography</h2>
                     <p>Contact us today to find out how we can help your business or project, consultations are always free!</p>
-                    <a class="btn btn-main btn-solid-border" href="#">Request A Consultation</a>
+                    <a class="btn btn-main btn-solid-border scrollto" href="#contact">Request A Consultation</a>
                 </div>
             </div>
         </div>
