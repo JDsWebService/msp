81beb6ea7150c47c935f8c826e239d41249edd53
Jonathan Robinson <jdswebservice@gmail.com>
2020-09-07 12:14:25 -0400

    Added Droners Badge To Footer

--- resources/views/partials/app/footer.blade.php
+++ resources/views/partials/app/footer.blade.php
@@ -10,19 +10,25 @@
                         <li><a href="#service" class="scrollto">Services</a></li>
                     </ul>
                 </div>
-                <p class="copyright">
-                    &copy; {{ config('app.name', 'Maine Sky Pixels') }} {{ \Carbon\Carbon::now()->year }} - All Rights Reserved
-                    <br>
-                    Developed by <a href="http://jdswebservice.com/" target="_blank">JD's Web Service</a>
-                    <span class="text-muted">#1319099</span>
-                    <br>
-                    @guest
-                        <a href="{{ route('login') }}">Webmaster Login</a>
-                    @endguest
-                    @auth
-                        <a href="{{ route('admin.dashboard') }}">Admin Dashboard</a>
-                    @endauth
-                </p>
+                <div class="row justify-content-center">
+                    <div class="col-sm-4">
+                        <img src="/imgs/droners_badge.png" alt="" class="w-50 img-fluid mb-2">
+                        <p class="copyright">
+                            &copy; {{ config('app.name', 'Maine Sky Pixels') }} {{ \Carbon\Carbon::now()->year }} - All Rights Reserved
+                            <br>
+                            Developed by <a href="http://jdswebservice.com/" target="_blank">JD's Web Service</a>
+                            <span class="text-muted">#1319099</span>
+                            <br>
+                            @guest
+                                <a href="{{ route('login') }}">Webmaster Login</a>
+                            @endguest
+                            @auth
+                                <a href="{{ route('admin.dashboard') }}">Admin Dashboard</a>
+                            @endauth
+                        </p>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
