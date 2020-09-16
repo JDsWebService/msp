28e5164a654d1fdbd5873ceaf68d7d5e69c14e7d
Jonathan Robinson <jdswebservice@gmail.com>
2020-09-16 15:10:36 -0400

    Added Migration for Slug Field on Portfolio Table

---
+++ database/migrations/2020_09_16_190904_add_slug_field_to_portfolio_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSlugFieldToPortfolioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('portfolio', function (Blueprint $table) {
+            // Add the slug field to the Portfolio table
+            $table->string('slug')->after('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('portfolio', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+    }
+}
